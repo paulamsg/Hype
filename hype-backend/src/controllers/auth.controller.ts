f9d5de5348cf8aa6f5756ec702483391bbf1afb9
Paula Meseguer Martínez <paulamsg743@gmail.com>
2026-02-24 10:39:45 +0100

    update auth.controller with register

---
+++ hype-backend/src/controllers/auth.controller.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import prisma from "../config/database"
+
+export const register = async (req: Request, res: Response) => {
+    try{
+        const {name, lastName, email, password, location} = req.body;
+        const emailExist = await prisma.user.findUnique({
+            where: {email}
+        })
+        
+        if(emailExist){
+            return res.status(400).json({message: "El email ya está registrado"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const baseUsername = `${name.toLowerCase()}${lastName.toLowerCase()}`.replace(/\s/g, "")
+        const randomNum = Math.floor(Math.random() * 9000) + 1000
+        const username = `${baseUsername}_${randomNum}` 
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                lastName,
+                username: username,
+                email,
+                password: hashedPassword,
+                location
+            }
+        });
+
+        const userToken = jwt.sign(
+            {userId: user.id},
+            process.env.JWT_SECRET as string,
+            {expiresIn: "7d"}
+        );
+
+        res.status(201).json({
+            message:"Usiario creado correctamente",
+            userToken, 
+            user : {
+                id: user.id,
+                name: user.name,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    }catch (error){
+        res.status(500).json({message: "Error interno del servidor"})
+    }
+}
+
+export const login = () => {
+
+}
