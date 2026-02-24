b947e76d695353511ddb4d8312cd5ee930cb4e8f
Paula Meseguer Martínez <paulamsg743@gmail.com>
2026-02-24 14:16:46 +0100

    update server.ts to add authRoutes

--- hype-backend/src/server.ts
+++ hype-backend/src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
-
+import authRoutes from "./routes/auth.routes" 
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+//Rutas
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
