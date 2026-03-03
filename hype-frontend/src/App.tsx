3bebd43d51135ae48abd30939293d6c5624ea058
Paula Meseguer Martínez <paulamsg743@gmail.com>
2026-03-03 18:11:15 +0100

    added scss and StyleGuide file

--- hype-frontend/src/App.tsx
+++ hype-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Login from "./pages/LoginPage";
 import Register from "./pages/RegisterPage";
 import Discover from "./pages/DiscoverPage";
+import StyleGuide from "./pages/styleguide/StyleGuide";
 import ProtectedRoute from "./components/ProtectedRoute"
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      
+      <Route path="/style-guide" element={<StyleGuide />} />
       {/* Rutas protegidas */}
         <Route element={<ProtectedRoute />}>
           <Route path="/descubre" element={<Discover />} />
---
+++ hype-frontend/src/pages/styleguide/StyleGuide.tsx
@@ -0,0 +1,51 @@
+const StyleGuide = () =>{
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Elemento</th>
+                    <th>Ejemplo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>h1</td>
+                    <td><h1>Esto es un h1</h1></td>
+                </tr>
+                <tr>
+                    <td>h2</td>
+                    <td><h2>Esto es un h2</h2></td>
+                </tr>
+                <tr>
+                    <td>h3</td>
+                    <td><h3>Esto es un h3</h3></td>
+                </tr>
+                <tr>
+                    <td>h4</td>
+                    <td><h4>Esto es un h4</h4></td>
+                </tr>
+                <tr>
+                    <td>h5</td>
+                    <td><h5>Esto es un h5</h5></td>
+                </tr>
+                <tr>
+                    <td>h6</td>
+                    <td><h6>Esto es un h6</h6></td>
+                </tr>
+                <tr>
+                    <td>p</td>
+                    <td><p>Esto es un párrafo</p></td>
+                </tr>
+                <tr>
+                    <td>Botón</td>
+                    <td><button>Esto es un botón</button></td>
+                </tr>
+                <tr>
+                    <td>Input</td>
+                    <td><input placeholder="escribe aquí"></input></td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+export default StyleGuide;
