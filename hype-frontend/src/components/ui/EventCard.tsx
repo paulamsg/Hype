e9a8604627e836a4e6ab1fe40eced8e3a230be15
Paula Meseguer Martínez <paulamsg743@gmail.com>
2026-03-10 18:57:43 +0100

    new json events

--- hype-frontend/src/components/ui/EventCard.tsx
+++ hype-frontend/src/components/ui/EventCard.tsx
@@ -1,12 +1,12 @@
 import type { Event } from "../../types/event.types";
-const EventCard = ({name, date, venue, image,genre, subGenre}:Event) =>{
+const EventCard = ({name, date, venue, category, image, genre}:Event) =>{
     return (
         <div className="event-card">
             <div className="event-card__img">
                 <img src= {image}></img>
             </div>
             <div className="event-card__info">
-                <p>{genre} - {subGenre}</p>
+                <p>{category} - {genre}</p>
                 <p><b>{name}</b></p>
                 <p>{date} - {venue}</p>
             </div>
